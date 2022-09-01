073f69e76cf0cfcb7b5d64d7d2a33e8dbd1d0da5
EnriqueIzel2 <eleaoizel@gmail.com>
2022-08-31 21:52:16 -0400

    feat: create listener to get text from Edits components

--- app/src/main/java/com/enrique/imccalculator/MainActivity.kt
+++ app/src/main/java/com/enrique/imccalculator/MainActivity.kt
@@ -11,5 +11,19 @@ class MainActivity : AppCompatActivity() {
     super.onCreate(savedInstanceState)
     binding = ActivityMainBinding.inflate(layoutInflater)
     setContentView(binding.root)
+
+    val btn_calculate = binding.btnCalculate
+    val txt_message = binding.txtMessage
+
+    btn_calculate.setOnClickListener {
+      val editWeight = binding.editWeight.text.toString()
+      val editHeight = binding.editHeight.text.toString()
+
+      if (editWeight.isEmpty() || editHeight.isEmpty()) {
+        txt_message.setText("Preencha todos os campos")
+      } else {
+
+      }
+    }
   }
 }
